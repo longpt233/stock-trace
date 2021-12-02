5ebec13e190a27fdd72ac9570cd5baba66449eff
Phan Thanh Long <driverofl01@gmail.com>
2021-12-03 00:22:41 +0700

    Tung crawl ho cai nha

--- airflow/dags/tasks/db/monggodb.py
+++ airflow/dags/tasks/db/monggodb.py
@@ -1,62 +1,41 @@
 from pymongo import MongoClient
+import csv
 
 MONGO_URL = "mongodb+srv://longpt:longpt@cluster-longpt.ocem8.mongodb.net/test?authSource=admin&replicaSet=atlas-qdmyf4-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true";
 myclient = MongoClient(MONGO_URL)
-db = myclient["stock"]
+db = myclient["stock_db"]
 
+def get_com_name_v2():
 
-def get_all_company_v1():
-
-    collection = db['stock_name_v1']
+    collection = db['com_name_v2']
     all_document = collection.find({})     # get all document 
 
     list_name_price = []
     
     for dict_document in all_document:           # document is a dict
-        list_name_price.append({        
-            "name" : dict_document.get("name"),
-            "price": dict_document.get("price")
-        })
+        list_name_price.append(dict_document.get("data"))
  
     return list_name_price
 
+def push_com_name_v2():
 
-def get_all_company_v2():
+    filename ="./com_name_v2.csv"
+    rows =[]
+    with open(filename, 'r') as csvfile: 
+        csvreader = csv.reader(csvfile)
+        fields = next(csvreader)   # extracting field names through first row
+        for row in csvreader:      # extracting each data row one by one
+            rows.append(row[1])
 
-    collection = db['stock_name_v2']
-    all_document = collection.find({})     # get all document 
+    stock_name_json = {
+        "name" :"list company 20-35 ", 
+        "data" : rows
+    }
 
-    list_name_price = []
-    
-    for dict_document in all_document:           # document is a dict
-        list_name_price.append({        
-            "name" : dict_document.get("name"),
-            "price": dict_document.get("price")
-        })
- 
-    return list_name_price
-
-def push_all_company_v1(list_company_price):
-    pass
-
-def push_all_company_v2(list_company_price):
-
-    
-    pass
-
-# minimize stock_name_v2
-def getTargetCompany():
-    return
+    db["com_name_v2"].insert_one(stock_name_json)
 
 
-def pushTargetCompany():
-    return
+if __name__ == "__main__":
 
-
-def getAllCompanyAndPrice():
-    pass
-
-
-def pushAllCompanyAndPrice():
-    pass
-
+    # push_com_name_v2()
+    print(get_com_name_v2())
--- crawl-module/crawl/push_data.py
+++ crawl-module/crawl/push_data.py
@@ -2,28 +2,50 @@ import json
 import pandas as pd
 import sys, getopt, pprint
 from pymongo import MongoClient
-
 import csv
 import os 
 
-CSV_DATA_PATH = './'
-all_csv_stock_path = [os.path.join(CSV_DATA_PATH, f'{x}') for x in os.listdir(CSV_DATA_PATH)]
-
-MONGO_URL = "mongodb+srv://longpt:lonpt@cluster-longpt.ocem8.mongodb.net/test?authSource=admin&replicaSet=atlas-qdmyf4-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true"
+MONGO_URL = "mongodb+srv://longpt:longpt@cluster-longpt.ocem8.mongodb.net/test?authSource=admin&replicaSet=atlas-qdmyf4-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true"
 myclient = MongoClient(MONGO_URL)
 db = myclient["stock_db"]
 
-for stock_path  in all_csv_stock_path : 
-    results = []
-    with open(stock_path) as csvfile:
-        reader = csv.reader(csvfile)  
-        for row in reader: # each row is a list
-            results.append(row)
+def push_com_name_v1():
+    f = open('./cate-company.json')
+    data = json.load(f) 
+    company_list =[]
+    for key, value in data.items():
+        company_list.extend(value)
 
-    name_stock = str(stock_path).split("/")[-1].replace(".csv","")
-    a_stock_json = {
-        "code" : name_stock,
-        "data" : results
+    stock_name_json = {
+        "name" :"list company", 
+        "data" : company_list
     }
-    # print(results)
-    db["com_name_v1"].insert(a_stock_json)
+
+    db["com_name_v1"].insert_one(stock_name_json)
+    
+    pass  
+
+def push_com_price_v1(): 
+
+    CSV_DATA_PATH = './'
+    all_csv_stock_path = [os.path.join(CSV_DATA_PATH, f'{x}') for x in os.listdir(CSV_DATA_PATH)]
+    for stock_path  in all_csv_stock_path : 
+        results = []
+        with open(stock_path) as csvfile:
+            reader = csv.reader(csvfile)  
+            for row in reader: # each row is a list
+                results.append(row)
+
+        name_stock = str(stock_path).split("/")[-1].replace(".csv","")
+        a_stock_json = {
+            "code" : name_stock,
+            "data" : results
+        }
+        # print(results)
+        db["com_price_v1"].insert_one(a_stock_json)
+
+
+if __name__ == "__main__":
+
+    push_com_name_v1()
+
